ce6b20e80ecb86fd9ebb0389837ebc07455351b6
mentos1386 <tine.jozelj@outlook.com>
2015-03-16 21:35:13 +0100

    Improved Posts for Photon Theme

--- resources/views/themes/Photon/frontend/frontpage/posts/partial/post.blade.php
+++ resources/views/themes/Photon/frontend/frontpage/posts/partial/post.blade.php
@@ -2,9 +2,16 @@
     <div class="row 150%">
         <div class="12u 12u$(medium)">
             <header class="major">
-                <a href="{{ Themes::postUrl($post['id']) }}"><h2>{{ $post['title'] }}</h2></a>
+                <a href="{{ Themes::postUrl($post['id']) }}"><h2><span class="fa fa-link"></span> {{ $post['title'] }}
+                    </h2></a>
             </header>
             <p>{{ $post['body'] }}</p>
+            <ul class="tags">
+                @foreach($post['tag'] as $tag)
+                    <li><span class="fa fa-tag"></span> {{ $tag['name'] }}</li>
+                @endforeach
+            </ul>
+            <p style="float:right;">{{ $post['user']['username'] }} <span class="fa fa-user"></span></p>
         </div>
     </div>
 </div>
